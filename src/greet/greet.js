f3fd89497afb957d0d319d328b7f15acff93e62a
ZanaSiaka <zanacoul21@gmail.com>
2024-03-09 07:26:19 +0000

    Wai

---
+++ src/greet/greet.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+function Greet(){
+    return <h1> Hello à tous !!!</h1>
+}
+
+export default Greet;
---
+++ src/greet/greet2.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class Greet2 extends Component{
+    render(){
+        let a = "Bienvenue chers fans"
+        return <h1>{a}</h1>
+    }
+}
+
+export default Greet2
